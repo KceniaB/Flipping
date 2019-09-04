0978590f750c6abc5daa9c063799ee1908b86f96
DarioSarra <dariosarra@gmail.com>
2019-09-04 16:48:04 +0100

    removing some warning about DataFrames use

--- src/new_tools.jl
+++ src/new_tools.jl
@@ -87,24 +87,24 @@ end
 """
 
 function process_streaks(df::DataFrames.AbstractDataFrame; photometry = false)
-    columns_list = [:MouseID, :Gen, :Drug, :Day, :Daily_Session, :Box, :Stim_Day, :Condition, :ExpDay, :Area, :Session];
+    dayly_vars_list = [:MouseID, :Gen, :Drug, :Day, :Daily_Session, :Box, :Stim_Day, :Condition, :ExpDay, :Area, :Session];
     booleans=[:Reward,:Side,:SideHigh,:Stim,:Wall,:Correct,:Stim_Day]#columns to convert to Bool
     for x in booleans
-        df[x] = eltype(df[x]) == Bool ? df[x] : occursin.(df[x],"true")
+        df[!,x] = eltype(df[!,x]) == Bool ? df[!,x] : occursin.(df[!,x],"true")
     end
     streak_table = by(df, :Streak) do dd
         dt = DataFrame(
         Num_pokes = size(dd,1),
-        Num_Rewards = length(findall(dd[:Reward].==1)),
+        Num_Rewards = length(findall(dd[!,:Reward].==1)),
         Start_Reward = dd[1,:Reward],
-        Last_Reward = findlast(dd[:Reward] .== 1).== nothing ? 0 : findlast(dd[:Reward] .== 1),
-        Prev_Reward = findlast(dd[:Reward] .== 1).== nothing ? 0 : findprev(dd[:Reward] .==1, findlast(dd[:Reward] .==1)-1),
+        Last_Reward = findlast(dd[!,:Reward] .== 1).== nothing ? 0 : findlast(dd[!,:Reward] .== 1),
+        Prev_Reward = findlast(dd[!,:Reward] .== 1).== nothing ? 0 : findprev(dd[!,:Reward] .==1, findlast(dd[!,:Reward] .==1)-1),
         Trial_duration = (dd[end,:PokeOut]-dd[1,:PokeIn]),
         Start = (dd[1,:PokeIn]),
         Stop = (dd[end,:PokeOut]),
-        Pre_Interpoke = size(dd,1) > 1 ? maximum(skipmissing(dd[:Pre_Interpoke])) : missing,
-        Post_Interpoke = size(dd,1) > 1 ? maximum(skipmissing(dd[:Post_Interpoke])) : missing,
-        PokeSequence = [SVector{size(dd,1),Bool}(dd[:Reward])],
+        Pre_Interpoke = size(dd,1) > 1 ? maximum(skipmissing(dd[!,:Pre_Interpoke])) : missing,
+        Post_Interpoke = size(dd,1) > 1 ? maximum(skipmissing(dd[!,:Post_Interpoke])) : missing,
+        PokeSequence = [SVector{size(dd,1),Bool}(dd[!,:Reward])],
         Stim = dd[1,:Stim],
         StimFreq = dd[1,:StimFreq],
         Wall = dd[1,:Wall],
@@ -116,14 +116,14 @@ function process_streaks(df::DataFrames.AbstractDataFrame; photometry = false)
         Side = dd[1,:Side],
         ReverseStreak = dd[1,:ReverseStreak]
         )
-        for s in columns_list
+        for s in dayly_vars_list
             if s in names(df)
-                dt[s] = df[1, s]
+                dt[!,s] .= df[1, s]
             end
         end
         return dt
     end
-    streak_table[:Prev_Reward] = [x .== nothing ? 0 : x for x in streak_table[:Prev_Reward]]
+    streak_table[!,:Prev_Reward] = [x .== nothing ? 0 : x for x in streak_table[:Prev_Reward]]
     streak_table[:AfterLast] = streak_table[:Num_pokes] .- streak_table[:Last_Reward];
     streak_table[:BeforeLast] = streak_table[:Last_Reward] .- streak_table[:Prev_Reward].-1;
     prov = lead(streak_table[:Start],default = 0.0) .- streak_table[:Stop];
